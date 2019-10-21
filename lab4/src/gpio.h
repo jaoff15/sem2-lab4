30b6d49b8b4e764f0cd67a1582404bb5a1836ffe
jaoff15 <jaoff15@student.sdu.dk>
2019-10-21 13:13:41 +0200

    Lab4 added

---
+++ lab4/src/gpio.h
@@ -0,0 +1,56 @@
+/*
+ * Gpio.h
+ *
+ *  Created on: Sep 24, 2019
+ *      Author: jacoboffersen
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <unistd.h>
+#include "sleep.h"
+
+typedef enum {
+	in, out
+} Direction;
+
+class Gpio {
+public:
+	Gpio();
+	virtual ~Gpio();
+
+	/* Set IO pin number */
+	void setPinNumber(const std::string pin);
+
+	/* Set GPIO path if it is different than the default (/sys/class/gpio)*/
+	void setGpioPath(const std::string path);
+
+	/* Export pin */
+	int exportPin();
+
+	/* Unexport pin */
+	int unexportPin();
+
+	/* Set pin direction */
+	int setDirection(const Direction dir);
+
+	/* Set pin value */
+	int setValue(const bool value);
+
+	/* Read pin value */
+	int getValue(bool *value);
+
+private:
+	Sleep sleep;
+	std::string gpio_path_;  // Path to the GPIO
+	std::string path_;		 // Path to the pin itself
+	std::string pin_;		 // Pin number
+
+};
+
+#endif /* GPIO_H_ */
---
+++ lab4/src/lab4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab4.cpp
+// Author      : Jacob Offersen
+// Version     :
+// Copyright   : Your copyright notice
+// Description : 
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
